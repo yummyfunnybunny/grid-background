544df49b59a35a601f1297ac03ad3b6367170275
jake nichols <yummyfunnybunny13@gmail.com>
2021-01-24 23:20:48 -0500

    created more option variables to be implemented next

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ window.addEventListener("load", () => {
 
   // general Options
   const cellWidth = 100; // pixels
-  const clickCellSpeed = 50; // miliseconds
-  const clickCellDelay = 500; // miliseconds
+  const borderSize = 1;
+  const borderRadius = 0; // percentage
+  const borderColor = [0, 0, 21, 1]; // [hue, saturation, lightness, opacity]
+  const backgroundColor = [0, 0, 15, 1]; // [hue, saturation, lightness, opcaity]
+  const cellBounceX = 0; // percentage (transform: translateX(0))
+  const cellBounceY = -20; // percentage (transform: translateY(-20%))
+
+  const transBorderRadius = 0;
+  const transBordorColor = [293, 95, 71, 1];
+  const transBackgroundColor = [293, 95, 71, 0.5];
 
   // Mouseover Options
   const mouveOverEffect = false;
 
   // Click Options
   const clickEffect = false;
+  const clickSpeed = 0.05; // seconds
+  const clickSwell = 0.5; // seconds
 
   // Random Options
   const randomEffect = false;
@@ -25,7 +35,14 @@ window.addEventListener("load", () => {
   const waveScatter = 1; // seconds
 
   // ColorOptions
-  const randomColors = false;
+  const randomColors = true;
+  const randomHue = true;
+  const randomSaturation = false;
+  const randomLightness = false;
+  // const setColor = [293, 95, 71];
+  const hue = 293;
+  const saturation = 95;
+  const lightness = 71;
 
   // ANCHOR -- Initialize Grid --
   let width, height, columns, rows;
@@ -70,28 +87,28 @@ window.addEventListener("load", () => {
     //   ANCHOR -- Mouse click Effect --
     if (clickEffect) {
       el.addEventListener("click", function () {
-        let _cellRow = el.classList[0].split("-")[1];
-        let _cellCol = el.classList[0].split("-")[2];
+        const _cellRow = el.classList[0].split("-")[1];
+        const _cellCol = el.classList[0].split("-")[2];
 
         // Cells Above Clicked Cell
         for (let i = _cellRow; i >= 0; i--) {
-          let _cell = document.querySelector(`.cell-${i}-${_cellCol}`);
-          clickCellEffect(_cellRow, _cell, clickCellSpeed, clickCellDelay, i);
+          const _cell = document.querySelector(`.cell-${i}-${_cellCol}`);
+          clickCellEffect(_cellRow, _cell, i);
         }
         // Cells Below Clicked Cell
         for (let i = _cellRow; i < rows; i++) {
-          let _cell = document.querySelector(`.cell-${i}-${_cellCol}`);
-          clickCellEffect(_cellRow, _cell, clickCellSpeed, clickCellDelay, i);
+          const _cell = document.querySelector(`.cell-${i}-${_cellCol}`);
+          clickCellEffect(_cellRow, _cell, i);
         }
         // Cells left of Clicked Cell
         for (let i = _cellCol; i >= 0; i--) {
-          let _cell = document.querySelector(`.cell-${_cellRow}-${i}`);
-          clickCellEffect(_cellCol, _cell, clickCellSpeed, clickCellDelay, i);
+          const _cell = document.querySelector(`.cell-${_cellRow}-${i}`);
+          clickCellEffect(_cellCol, _cell, i);
         }
         // Cells right of Clicked Cell
         for (let i = _cellCol; i < columns; i++) {
-          let _cell = document.querySelector(`.cell-${_cellRow}-${i}`);
-          clickCellEffect(_cellCol, _cell, clickCellSpeed, clickCellDelay, i);
+          const _cell = document.querySelector(`.cell-${_cellRow}-${i}`);
+          clickCellEffect(_cellCol, _cell, i);
         }
       });
     }
@@ -197,14 +214,14 @@ window.addEventListener("load", () => {
   }
 
   //   ANCHOR -- Click Cell Effect --
-  function clickCellEffect(cells, cell, speed, delay, i) {
+  function clickCellEffect(cells, cell, i) {
     setTimeout(function () {
       turnCellOn(cell);
-    }, speed * Math.abs(cells - i));
+    }, clickSpeed * 1000 * Math.abs(cells - i));
 
     setTimeout(function () {
       turnCellOff(cell);
-    }, delay + speed * Math.abs(cells - i));
+    }, clickSwell * 1000 + clickSpeed * 1000 * Math.abs(cells - i));
   }
 
   // ANCHOR -- Turn Cell On --
@@ -227,20 +244,50 @@ window.addEventListener("load", () => {
   //   this.classList.toggleClass("animating");
   // }
 
+  // ANCHOR -- Set Color --
+  function setColor(cell) {
+    if (randomColors) {
+      setRamdonColor(cell);
+    } else {
+      cell.style.borderColor = `hsl(${hue},${saturation}%,${lightness}%)`;
+    }
+  }
   // ANCHOR -- Set Random Color --
   function setRandomColor(cell) {
     // hsl(293, 95%, 71%);
-    const hue = Math.floor(Math.random() * 360);
-    const col1 = Math.floor(Math.random() * 100);
-    const col2 = Math.floor(Math.random() * 100);
-    // cell.style.borderColor = `hsl(${hue},${col1}%,${col2}%)`;
-    cell.style.borderColor = `hsl(${hue},95%,71%)`;
+    let _hue, _saturation, _lightness;
+    if (randomHue) {
+      _hue = Math.floor(Math.random() * 360);
+    } else {
+      _hue = hue;
+    }
+    if (randomSaturation) {
+      _saturation = Math.floor(Math.random() * 100);
+    } else {
+      _saturation = saturation;
+    }
+    if (randomLightness) {
+      _lightness = Math.floor(Math.random() * 100);
+    } else {
+      _lightness = lightness;
+    }
+    // const saturation = Math.floor(Math.random() * 100);
+    // const lightness = Math.floor(Math.random() * 100);
+    // cell.style.borderColor = `hsl(${hue},95%,71%)`;
+    cell.style.borderColor = `hsl(${_hue},${_saturation}%,${_lightness}%)`;
   }
 
-  // ANCHOR -- Set Random Color --
+  // ANCHOR -- Reset Color --
   function resetColor(cell) {
     cell.style.borderColor = `hsl(0, 0%, 21%)`;
   }
 });
 
 // !SECTION ====================================
+
+// TODO
+/*
+- fix the mouseover and click functions
+- add a HUD for changing the options? *this could take awhile*
+- fix animation end toggling
+*/
